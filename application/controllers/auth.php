49fd8387efe1ec63b89c233a757d10e0260fa5c1
unknown <Mureithi@.(none)>
2012-04-27 15:11:42 +0300

    added a comment

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -20,7 +20,7 @@ class Auth extends MY_Controller {
 						->findOneBy(array('username' => $this -> input -> post('username'), 'password' => $this -> input -> post('secret')));
      
 			
-		//if ($user[0]['id'] >0) {
+		//if ($user[0]['id'] >0) {//for array based
 	    if ($user) {
 			//$user = $this->em->find('models\SystemUser',$user[0]['id']); //pass to the object
 
