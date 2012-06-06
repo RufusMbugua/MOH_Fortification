c97294c836c6758c39a29ea03b60ce3ffe4c0cf2
John Adamsy <adamsy.opiyo@gmail.com>
2012-06-06 10:37:19 +0300

    Get Session Info methods

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,7 +3,8 @@
 
 class  MY_Controller  extends  CI_Controller  {
 
-public $em, $response, $theForm, $rowsInserted, $executionTime,$data;
+public $em, $response, $theForm, $rowsInserted, $executionTime,$data,
+      $selectCompManufacturers,$selectPremixType, $selectIodizationCentre;
 
 function __construct()  {
 		parent::__construct();
@@ -15,11 +16,44 @@ function __construct()  {
 		$this->response='';
 		$this->theForm='';
 		$this->data='';
+		$this->selectCompManufacturers='';
+		$this->selectPremixType='';
+		$this->selectIodizationCentre='';
+		
+		$this->getCompManufacturerNamesAndIds();
+		$this->getPremixTypesAndIds();
+		$this->getIodizationCentreNames();
 	}
 
-function  getRepositoryByFormName($form){
-	$this->the_form=$this->em->getRepository($form);
-	return $this->theForm;
-}
+	function  getRepositoryByFormName($form){
+		$this->the_form=$this->em->getRepository($form);
+		return $this->theForm;
+	}
+
+	public function getCompManufacturerNamesAndIds(){/*obtained from the session data*/
+		       if($this -> session -> userdata('compoundManufacturers'))
+				foreach($this -> session -> userdata('compoundManufacturers') as $key=>$value){
+				$this->selectCompManufacturers.= '<option value="'.$value['manufacturerId'].'">'.$value['manufacturerCompName'].'</option>'.'<br />';
+				}
+				
+				return $this->selectCompManufacturers;
+			
+		}
+	
+	public function getPremixTypesAndIds(){  /*obtained from the session data*/
+	       if($this -> session -> userdata('premixType'))
+			foreach($this -> session -> userdata('premixType') as $key=>$value){
+			$this->selectPremixType.= '<option value="'.$value['productId'].'">'.$value['productName'].'</option>'.'<br />';
+			}
+		return $this->selectPremixType;
+	}
+	
+	public function getIodizationCentreNames(){ /*obtained from the session data*/
+	       if($this -> session -> userdata('iodizationCentre'))
+			foreach($this -> session -> userdata('iodizationCentre') as $key=>$value){
+			$this->selectIodizationCentre.= '<option value="'.$value['factoryNumber'].'">'.$value['factoryName'].'</option>'.'<br />';
+			}
+		return $this->selectIodizationCentre;
+	}
 
 }  
