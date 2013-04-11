544e356ca706adcd86c5b323222801fd46d33cb8
RufusMbugua <mbuguarufus@gmail.com>
2013-04-11 17:34:41 +0300

    Edits

--- application/views/sections/header.php
+++ application/views/sections/header.php
@@ -1,21 +1,20 @@
 <section class="banner">
-			<section class="logo">
-				<img src="<?php echo base_url(); ?>images/coat_of_arms-resized.png" />
+	<section class="logo">
+		<img src="<?php echo base_url(); ?>images/coat_of_arms-resized.png" />
 
-			</section>
-			<section class="credentials">
-				<section class="title">
-					Ministry Of Public Health and Sanitation
-				</section>
-				<section class="subtitle">
-					Department of Family Health<p></p>
-				</section>
-				<section class="division">
-					Division of Nutrition<p></p><p></p>
-				</section>
-			</section>
-			<section class="date">
-				
-				 <?php echo date("l, F d Y"); ?>
-			</section>
+	</section>
+	<section class="credentials">
+		<section class="title">
+			Ministry Of Public Health and Sanitation
+		</section>
+		<section class="subtitle">
+			Department of Family Health<p></p>
+		</section>
+		<section class="division">
+			Division of Nutrition<p></p><p></p>
+		</section>
+	</section>
+	<section class="date">
+		<?php echo date("l, F d Y"); ?>
+	</section>
 </section>
